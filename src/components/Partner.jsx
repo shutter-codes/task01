beea87a7283da1ab8a3c9179f86fd09642d47e04
Naitik Sharma <snaitik0238@gmail.com>
2023-11-17 19:29:50 +0530

    api added

--- src/components/Partner.jsx
+++ src/components/Partner.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Partner() {
+  const [partners, setPartners] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/api/partner")
+      .then((response) => {
+        console.log("printing response");
+        console.log(response.data.partner.length);
+        setPartners(response.data.partner);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div class="card w-96 bg-base-100 shadow-xl mb-8">
-      <figure class="px-10 pt-10">
-        <img
-          src="https://plus.unsplash.com/premium_photo-1698276989965-d00e2fff0078?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Shoes"
-          class="rounded-xl"
-        />
-      </figure>
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions">
-          <button class="btn btn-primary">Buy Now</button>
+    <div className="flex flex-wrap justify-center">
+      {partners.map((partner) => (
+        <div key={partner.id} className="card w-96 bg-base-100 shadow-xl mb-8 mx-4">
+          <div className="card-body items-center text-center">
+            <h1>Partner Length: {partners.length}</h1>
+            <h3>{partner.partnerName}</h3>
+            <p>{partner.desc}</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
