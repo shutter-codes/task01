51ab2d2bb8874ba7f17e07e49919320c4eaf39e1
Naitik Sharma <snaitik0238@gmail.com>
2023-11-18 11:31:00 +0530

    about api added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,30 +2,37 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function About() {
-  const [aboutContent, setAboutContent] = useState("");
+  const [aboutContents, setAboutContent] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://127.0.0.1:5000/about");
-        console.log(response.data);
-        setAboutContent(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+    axios
+      .get("http://127.0.0.1:5000/api/about")
+      .then((response) => {
+        console.log("printing about response");
+        console.log(response.data.about);
 
-    fetchData();
-  }, []); 
+        // Get the last item from the array
+        const lastItem = response.data.about[response.data.about.length - 1];
+
+        setAboutContent([lastItem]); // Set only the last item in the state
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
       <h1 className="text-3xl text-white my-6 py-3 font-bold flex justify-center items-center">
         About US
       </h1>
-      <p className="text-lg my-6 py-3 flex items-center justify-center text-center">
-        {aboutContent || "Loading..."}
-      </p>
+      {aboutContents.map((aboutContent) => (
+        <div key={aboutContent.id} className="card w-96 bg-base-100 shadow-xl mb-8 mx-4">
+          <div className="card-body items-center text-center">
+            <h3>{aboutContent.content}</h3>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
